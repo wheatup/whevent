ccc4d0b7102c69341f958e10f6fee5dbaf305cda
wheatup <acphun@gmail.com>
2019-11-18 19:03:06 +0900

    2.1.0

--- whevent.d.ts
+++ whevent.d.ts
@@ -12,7 +12,7 @@ declare module whevent {
 	 * @param self This boject
 	 */
 	@deprecated
-	export function bind(signal: string, func: function, self?: any);
+	export function bind(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Bind a signal with given function
@@ -20,7 +20,7 @@ declare module whevent {
 	 * @param func Function
 	 * @param self This boject
 	 */
-	export function on(signal: string, func: function, self?: any);
+	export function on(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, that function always get called first.
@@ -29,14 +29,14 @@ declare module whevent {
 	 * @param self This boject
 	 */
 	@deprecated
-	export function bindPriority(signal: string, func: function, self?: any);
+	export function bindPriority(signal: string|symbol, func: function, self?: any);
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, that function always get called first.
 	 * @param signal Signal
 	 * @param func Function
 	 * @param self This boject
 	 */
-	export function onPriority(signal: string, func: function, self?: any);
+	export function onPriority(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, the binding between this signal and this function will be destroyed.
@@ -45,14 +45,14 @@ declare module whevent {
 	 * @param self This boject
 	 */
 	@deprecated
-	export function bindOnce(signal: string, func: function, self?: any);
+	export function bindOnce(signal: string|symbol, func: function, self?: any);
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, the binding between this signal and this function will be destroyed.
 	 * @param signal Signal
 	 * @param func Function
 	 * @param self This boject
 	 */
-	export function onOnce(signal: string, func: function, self?: any);
+	export function onOnce(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, that function always get called first, after that the binding between this signal and this function will be destroyed.
@@ -62,14 +62,14 @@ declare module whevent {
 	 */
 	@deprecated
 
-	export function bindOncePriority(signal: string, func: function, self?: any);
+	export function bindOncePriority(signal: string|symbol, func: function, self?: any);
 	/**
 	 * Bind a signal with given function, once the signal is broadcasted, that function always get called first, after that the binding between this signal and this function will be destroyed.
 	 * @param signal Signal
 	 * @param func Function
 	 * @param self This boject
 	 */
-	export function onOncePriority(signal: string, func: function, self?: any);
+	export function onOncePriority(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Destroy the binding between given signal and function.
@@ -78,32 +78,45 @@ declare module whevent {
 	 * @param self This boject
 	 */
 	@deprecated
-	export function unbind(signal: string, func: function, self?: any);
+	export function unbind(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Destroy the binding between given signal and function.
 	 * @param signal Signal
 	 * @param func Function
-	 * @param self This boject
+	 * @param self This object
 	 */
-	export function off(signal: string, func: function, self?: any);
+	export function off(signal: string|symbol, func: function, self?: any);
 
 	/**
 	 * Destroy a signal, all bindings with this signal will be destroyed.
 	 * @param signal Signal
 	 */
-	export function destroy(signal: string);
+	export function destroy(signal: string|symbol);
 
 	/**
 	 * Broadcast a signal, triggers all binded functions.
 	 * @param signal Signal
 	 */
 	@deprecated
-	export function call(signal: string, ...data: any);
+	export function call(signal: string|symbol, ...data: any);
 
 	/**
 	 * Broadcast a signal, triggers all binded functions.
 	 * @param signal Signal
 	 */
-	export function emit(signal: string, ...data: any);
+	export function emit(signal: string|symbol, ...data: any);
+
+	/**
+	 * Unregister all event bindings associated with the listener
+	 * @param self Listener object
+	 */
+	@deprecated
+	export function unbindBy(self: any);
+
+	/**
+	 * Unregister all event bindings associated with the listener
+	 * @param self Listener object
+	 */
+	export function offBy(self: any);
 }
